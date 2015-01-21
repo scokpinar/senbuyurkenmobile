f6550764af52eaaef0fb2343227a30e8d2b193f5
samet <sametcokpinar@gmail.com>
2015-01-21 23:01:37 +0200

    first commit

---
+++ app/src/main/java/com/sam/senbuyurkenapp/FontCache.java
@@ -0,0 +1,28 @@
+package com.sam.senbuyurkenapp;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+import java.util.Hashtable;
+
+/**
+ * Created by SametCokpinar on 14/01/15.
+ */
+public class FontCache {
+
+    private static Hashtable<String, Typeface> fontCache = new Hashtable<String, Typeface>();
+
+    public static Typeface get(String name, Context context) {
+        Typeface tf = fontCache.get(name);
+        if(tf == null) {
+            try {
+                tf = Typeface.createFromAsset(context.getAssets(), name);
+            }
+            catch (Exception e) {
+                return null;
+            }
+            fontCache.put(name, tf);
+        }
+        return tf;
+    }
+}
