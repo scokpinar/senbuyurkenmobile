5010422f2d7911b17a4457046105822ab81d02db
samet <sametcokpinar@gmail>
2016-02-15 23:28:03 +0200

    User Logged-In state holder added.

--- app/src/main/java/com/sam/senbuyurkenmobile/LauncherActivity.java
+++ app/src/main/java/com/sam/senbuyurkenmobile/LauncherActivity.java
@@ -1,20 +1,14 @@
 package com.sam.senbuyurkenmobile;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.SyncHttpClient;
-
-import org.apache.http.Header;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -26,22 +20,23 @@ public class LauncherActivity extends Activity {
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_launcher);
 
-        /****** Create Thread that will sleep for 3 seconds *************/
         Thread background = new Thread() {
             public void run() {
 
                 try {
-                    // Thread will sleep for 3 seconds
-                    sleep(2 * 1000);
+                    // Thread will sleep for 0.5 seconds
+                    sleep(1 * 500);
 
-                    // After 3 seconds redirect to another intent
-                    //invokeWS();
+                    SharedPreferences sp = getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
+                    boolean userLoggedIn = sp.getBoolean("userLoggedIn", false);
 
-                    Intent i = new Intent(getBaseContext(), LoginActivity.class);
-                    //Intent i = new Intent(getBaseContext(), MainActivity.class);
-                    startActivity(i);
-
-                    //Remove activity
+                    if (userLoggedIn) {
+                        Intent i = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(i);
+                    } else {
+                        Intent i = new Intent(getBaseContext(), LoginActivity.class);
+                        startActivity(i);
+                    }
                     finish();
 
                 } catch (Exception e) {
@@ -74,54 +69,5 @@ public class LauncherActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void invokeWS() {
-        // Make RESTful webservice call using AsyncHttpClient object
-        SyncHttpClient client = new SyncHttpClient();
-
-
-        client.get(AppUtility.APP_URL + "rest/userRegistrationRest/checkURRService/", new JsonHttpResponseHandler() {
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-
-                try {
-                    JSONObject obj = response;
-                    // When the JSON response has status boolean value assigned with true
-                    if (obj.getBoolean("result")) {
-                        Intent i = new Intent(getBaseContext(), LoginActivity.class);
-                        startActivity(i);
-                    } else {
-                        Toast.makeText(getApplicationContext(), getApplicationContext().getString(R.string.register_existinguser_msg), Toast.LENGTH_LONG).show();
-                    }
-                } catch (JSONException e) {
-                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONArray errorResponse) {
-                // When Http response code is '404'
-                if (statusCode == 404) {
-                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
-                }
-                // When Http response code is '500'
-                else if (statusCode == 500) {
-                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
-                }
-                // When Http response code other than 404, 500
-                else {
-                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                super.onFailure(statusCode, headers, responseString, throwable);
-            }
-        });
-    }
-
 
 }
