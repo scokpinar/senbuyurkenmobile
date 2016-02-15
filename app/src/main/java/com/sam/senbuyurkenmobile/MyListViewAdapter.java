42d19910e83de088879cf245e42ac5df8197a5b9
samet <sametcokpinar@gmail>
2016-02-15 23:21:54 +0200

    View holder added for smooth scrolling.

--- app/src/main/java/com/sam/senbuyurkenmobile/MyListViewAdapter.java
+++ app/src/main/java/com/sam/senbuyurkenmobile/MyListViewAdapter.java
@@ -2,7 +2,6 @@ package com.sam.senbuyurkenmobile;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,24 +40,46 @@ public class MyListViewAdapter extends BaseAdapter {
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
+
         View vi = convertView;
-        if (convertView == null)
-            vi = inflater.inflate(R.layout.diary_entry, null);
 
-        TextView diary_entry_date = (TextView) vi.findViewById(R.id.diary_entry_date);
-        TextView diary_entry_content = (TextView) vi.findViewById(R.id.diary_entry_content);
-        ImageView diary_entry_image = (ImageView) vi.findViewById(R.id.diary_entry_image);
+        ViewHolder holder = new ViewHolder();
 
-        diary_entry_date.setText(data.get(position).getEntry_date());
-        diary_entry_content.setText(data.get(position).getEntry_content());
-        Bitmap bmp = data.get(position).getImage();
-        if (bmp == null) {
-            diary_entry_image.setMaxHeight(0);
-            diary_entry_image.setImageBitmap(null);
+        if (convertView == null) {
+            vi = inflater.inflate(R.layout.diary_entry, parent, false);
 
+            holder.diary_entry_title = (TextView) vi.findViewById(R.id.diary_entry_title);
+            holder.diary_entry_date = (TextView) vi.findViewById(R.id.diary_entry_date);
+            holder.diary_entry_image = (ImageView) vi.findViewById(R.id.diary_entry_image);
+            holder.diary_entry_content = (TextView) vi.findViewById(R.id.diary_entry_content);
+
+            vi.setTag(holder);
         } else {
-            diary_entry_image.setImageBitmap(bmp);
+            holder = (ViewHolder) vi.getTag();
+        }
+
+        DiaryEntryWrapper dew = data.get(position);
+
+        if (dew != null) {
+            holder.diary_entry_title.setText(dew.getEntry_title());
+            holder.diary_entry_date.setText(dew.getEntry_date());
+            if (!dew.getHasImage()) {
+                holder.diary_entry_image.setImageBitmap(null);
+                holder.diary_entry_image.setVisibility(View.GONE);
+            } else {
+                holder.diary_entry_image.setImageBitmap(dew.getImage());
+                holder.diary_entry_image.setVisibility(View.VISIBLE);
+            }
+            holder.diary_entry_content.setText(dew.getEntry_content());
         }
         return vi;
+    }
+
+
+    public class ViewHolder {
+        TextView diary_entry_title;
+        TextView diary_entry_date;
+        ImageView diary_entry_image;
+        TextView diary_entry_content;
     }
 }
